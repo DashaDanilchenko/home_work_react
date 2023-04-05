cc7c611a6acaf7a969c217559c9b80f147e2d288
dasha <dasha.danilchenko1990@gmail.com>
2023-04-05 14:38:10 +0300

    function check

--- src/components/CheckRadio.jsx
+++ src/components/CheckRadio.jsx
@@ -1,25 +1,20 @@
 const CheckRadio = ({setCheck, check}) => {
 
-    function checkAll() {
-        document.getElementById("all").checked = true;
-        document.getElementById("done").checked = false;
-        setCheck(true)
+    function checkAllOrDone() {
+        document.getElementById("all").checked = !check;
+        document.getElementById("done").checked = check;
+        setCheck(!check)
       }
     
-      function checkDone() {
-        document.getElementById("all").checked = false;
-        document.getElementById("done").checked = true;
-        setCheck(false)
-      }
 
   return (
     <div>
         <label htmlFor="all">
-          <input className="radio" type="radio" id="all" name="all" checked={check}  onChange={() => checkAll()}/> All
+          <input className="radio" type="radio" id="all" name="all" checked={check}  onChange={() => checkAllOrDone()}/> All
         </label>
 
         <label htmlFor="done">
-          <input className="radio" type="radio" id="done" name="done" onChange={() => checkDone()} /> Done
+          <input className="radio" type="radio" id="done" name="done" onChange={() => checkAllOrDone()} /> Done
         </label>
     </div>
   )
