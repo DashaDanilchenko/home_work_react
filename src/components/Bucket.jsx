1fc7a42076d78d0b404d36333675121c11abb7cc
dasha <dasha.danilchenko1990@gmail.com>
2023-04-14 15:51:00 +0300

    bucket

--- src/components/Bucket.jsx
+++ src/components/Bucket.jsx
@@ -1,12 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useContext } from 'react';
+import Context from "../Context";
 
 const Bucket = () => {
 
+  const { bucket } = useContext(Context);
+
+  
 
   return (
     <div className="bucket">
-      <button className="close" onClick={() => console.log (false)}><i className="fa-solid fa-xmark"></i></button>
+      
       <Link to="/">Home</Link>
+      <ul>
+        {bucket.map((item) => <li key={item.id}>
+          <span>{item.title}</span>
+          <button className="close" onClick={() => console.log (false)}><i className="fa-solid fa-xmark"></i></button>
+        </li>)}
+      </ul>
     </div>
   )
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,16 +1,62 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Context from "../Context";
 
 
 const Product = ({product}) => {
 
+    
+
+    const [bucket, setBucket] = useState({})
+
+    function str_gen() {
+        const allStr = '123456789QWERTYUIOPASDFGHJKLZXCVBNM';
+        let str = '';
+        for (let i = 0; i < 5; i++) {
+            let pos = Math.floor(Math.random() * allStr.length);
+            str += allStr.substring(pos,pos+1);
+        }
+        return str;
+    }
+
+    async function buyProduct(title) {
+        let id = str_gen()
+        const objectId = {id, title}
+        const res = await fetch("http://localhost:3000/bucket", { 
+              headers: {
+                "Content-Type": "application/json",
+              },
+              method: "POST", 
+              body: JSON.stringify(objectId),
+            });
+
+            const obj = await res.json()
+            setBucket([...bucket, obj])
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:3000/bucket')
+          .then(response => response.json())
+          .then(json => setBucket(json))
+      }, []);
+    
+      console.log(bucket)
+
+    const context = {
+        bucket,
+        setBucket,
+    }
+
 
     return (
-        <div className="list-item" >
+        <Context.Provider value={ context }>
+        <div className="list-item"  >
             <Link className="cursor" to={`/${product.id}`}>{ product.title }</Link>
             <span>{product.price}</span>
             <img src={product.img} alt="" />
-            <button className="button-buy" onClick={() => console.log( product )}>Buy</button>
+            <button className="button-buy" onClick={() => buyProduct( product.title )}>Buy</button>
         </div>
+        </Context.Provider> 
     )
 }
 
